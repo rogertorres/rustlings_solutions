a74d0b42c0793a253e9ce0a6e8d64c430ec4faa6
rogertorres <rogertps@gmail.com>
2021-01-25 18:59:51 -0300

    added the remaining exercises

---
+++ clippy/target/debug/deps/clippy2-24a73cc2111497f6.d
@@ -0,0 +1,5 @@
+/Users/roger/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-24a73cc2111497f6.rmeta: clippy2.rs
+
+/Users/roger/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-24a73cc2111497f6.d: clippy2.rs
+
+clippy2.rs:
---
+++ if/if1.rs
@@ -0,0 +1,34 @@
+// if1.rs
+
+pub fn bigger(a: i32, b: i32) -> i32 {
+    if a > b{
+        a
+    } else if b > a {
+        b
+    } else {
+        // Negative number means none of them is greater
+        -1
+    }
+
+    // Complete this function to return the bigger number!
+    // Do not use:
+    // - another function call
+    // - additional variables
+    // Execute `rustlings hint if1` for hints
+}
+
+// Don't mind this for now :)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ten_is_bigger_than_eight() {
+        assert_eq!(10, bigger(10, 8));
+    }
+
+    #[test]
+    fn fortytwo_is_bigger_than_thirtytwo() {
+        assert_eq!(42, bigger(32, 42));
+    }
+}
